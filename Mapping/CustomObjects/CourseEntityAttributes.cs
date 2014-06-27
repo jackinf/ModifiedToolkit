df8b165affe00b920b36bad147cf820186b73a7a
jevgenir <zeka.rum@gmail.com>
2014-06-27 17:38:55 +0300

    initial

---
+++ Mapping/CustomObjects/CourseEntityAttributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLToolkit.Mapping.CustomObjects
+{
+    public class CourseEntityAttributes : Hstore
+    {
+        public string code { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ Mapping/CustomObjects/Hstore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLToolkit.Mapping.CustomObjects
+{
+    public abstract class Hstore
+    {
+
+    }
+}
